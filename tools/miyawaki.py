bba83ae81ea147e88eb99a9b408f468d3558866d
Wes Ackerman <wesley.ackerman@gmail.com>
2016-10-19 15:30:49 -0600

    Added 'tools' directory and img generation functions

---
+++ tools/miyawaki.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def gen_img_set(size):
+    img_set = []
+    for x in range(0,size):
+        img_set.append(generate_img.gen_rand_img())
+    return np.asarray(img_set)
+
+def gen_rand_img():
+    image = np.ndarray((10,10), dtype=np.int)
+    for row in image:
+        for x in range(0,10):
+            row[x] = random.randint(0,1)
+    return image
